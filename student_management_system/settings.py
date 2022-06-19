4ea9c7e68ae5731dc9ca70df58548796a4cd6e12
HarshatD <>
2022-06-19 23:50:25 +0530

    Ip port and allowed host changed

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = '(i#*06f#keydy_fh17bf=$0f6v)^wr^l7*u4gq42m*sztu#2_m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['enigmatic-reaches-34965.herokuapp.com']
+ALLOWED_HOSTS = ['enigmatic-reaches-34965.herokuapp.com','127.0.0.1']
 
 
 # Application definition
